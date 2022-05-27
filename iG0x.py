c391ffbe1208cdb6237f2c916c48e5fa9ef537a0
HEXiN @hxn.py  insta <98489657+HXN2@users.noreply.github.com>
2022-05-27 16:19:34 +0300

    Create iG0x.py

---
+++ iG0x.py
@@ -0,0 +1,198 @@
+import os
+try:
+    import requests
+    import socket as s
+    import webbrowser
+    from sys import platform
+    import colorama
+    import time
+
+except ModuleNotFoundError:
+    os.system("pip install requests")
+    os.system("pip install socket")
+    os.system("pip install webbrowser")
+    os.system("pip install sys")
+    os.system("pip install platform")
+    os.system("pip install colorama")
+    os.system("pip install Today")
+    os.system("pip install Date")
+    os.system("pip install os")
+    os.system("pip install time")
+import webbrowser
+url = "https://t.me/HEXiN1K"
+webbrowser.open(url)
+from sys import platform
+from colorama import *
+import socket
+import requests
+from datetime import datetime
+import time
+
+now = datetime.now()
+
+current_time = now.strftime
+
+name = socket.gethostname()
+
+myHostName = socket.gethostname()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("\n===========================================================")
+name = ("""\n                    Hello sir """ +name+Style.RESET_ALL)
+print(name)
+
+import datetime
+e = datetime.datetime.now()
+print (Fore.CYAN+"""\n                   Your Day is """+"%s/%s/%s" % (e.day, e.month, e.year))
+print ("""\n                   The time is """+ "%s:%s:%s" % (e.hour, e.minute, e.second))
+
+host = (Fore.WHITE+"""\n                  Your platform is """+ platform+Style.RESET_ALL)
+print(host)
+
+api = "https://api.ipify.org/?format=json"
+req = requests.get(api)
+response = req.json()
+iP2 = response["ip"]
+
+IP = ("""\n                Your Local iP is """+Fore.RED+iP2+Style.RESET_ALL)
+print(IP)
+
+v = (Fore.RED+"""\n                    Your iNFO PROXY is\n"""+Style.RESET_ALL)
+
+print(v)
+print(s)
+
+print("\n===========================================================")
+time.sleep(1)
+os.system("clear")
+lib = input("""
+\n  من صنع هيكسن انستجرام @hxn.vb
+\n  BY HEXiN INSTA @hxn.vb
+\n  tarafından yapılmış HEXiN Instagram @hxn.vb
+\n
+===========================================================
+\n   من صنع هيكسن تيليجرام @HEXiN1K
+\n  BY HEXiN TELeGRaM @HEXiN1K
+\n  Bir telgraf HEXiN oluşturun @HEXiN1K
+\n ===========================================================
+ 
+ [1] DOWNLOAD LiP & UPDATE . تنزيل وتحديث المكاتب
+
+\n [2] SKiP DOWNLOAD & UPDATE LiP . تخطي تنزيل وتحديث المكاتب 
+
+\n [+] Please Choice >> """)
+
+if lib == "1":
+    try:
+        os.system('pip install requests')
+        os.system('pip install webbrowser')
+        os.system('pip install socket')
+        os.system('pip install sys')  
+        os.system('pip install colorama')
+        os.system('pip install time')
+        os.system('pip install discord')
+        os.system('cls' if os.name == 'nt' else 'clear')
+        pass
+    except:
+        pass
+else:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    pass
+import requests
+import colorama
+import webbrowser
+import socket
+import time as mm
+import sys as n
+def slow(M):
+	for c in M + '\n':
+		n.stdout.write(c)
+		n.stdout.flush()
+		mm.sleep(1. / 90)
+slow("""
+ ...........................................................  
+   
+  Coded BY HEXiN inStAGrAm : @hxn.vb
+  
+  TeLEGram programmer : @HEXiN1K 
+
+........................................................... 
+
+
+  تمت البرمجة بواسطة HEXiN  انستجرام : @hxn.vb
+
+    تيليجرام المبرمج : @HEXiN1K
+...........................................................
+                                                 """)
+os.system("clear")
+import requests
+from colorama import *
+import discord
+
+print(Fore.RED+""" \n 
+             ,----..       ,----..               
+    ,--,    /   /   \     /   /   \              
+  ,--.'|   |   :     :   /   .     :             
+  |  |,    .   |  ;. /  .   /   ;.  \,--,  ,--,  
+  `--'_    .   ; /--`  .   ;   /  ` ;|'. \/ .`|  
+  ,' ,'|   ;   | ;  __ ;   |  ; \ ; |'  \/  / ;  
+  '  | |   |   : |.' .'|   :  | ; | ' \  \.' /   
+  |  | :   .   | '_.' :.   |  ' ' ' :  \  ;  ;   
+  '  : |__ '   ; : \  |'   ;  \; /  | / \  \  \  
+  |  | '.'|'   | '/  .' \   \  ',  /./__;   ;  \ 
+  ;  :    ;|   :    /    ;   :    / |   :/\  \ ; 
+  |  ,   /  \   \ .'      \   \ .'  `---'  `--`  
+   ---`-'    `---`         `---`                 """)
+                                                                            
+
+print(Fore.RED+"\n {=} My old account has been deleted @hxn.py If you can remove the band contact me on Instagram here @hxn.vb ")
+
+usr1 = input (Fore.YELLOW+"\n [=] Enter user =>> : ")
+blacklist = ['hxn.py', 'hxn.py', '0xinsta', '0xinsta', '0xlnstagran', '0xlnstagran', 'hxn.py', '31421', '_gxdy', 'we62', '7lm.py', 'oz4p', 'd.0xc', 'cdiw_3', 'root.slaman', 'sir.sskip', 'j1r0', 'hxn.vb', 'hxn.vb']
+if usr1 in blacklist:
+        print(' [!] You cant searcing This Person')
+        exit(0)
+else:
+  api = f"https://jftv.pythonanywhere.com/IG-INFO/{usr1}"
+req = requests.get(api)
+response = req.json()
+ 
+a = response["response"]
+print(a)
+
+url = "https://instagram.com/hxn.vb/"
+webbrowser.open(url)
+
+message = f"""
+***Hi ~~SiR~~ [New iNFO]***
+Your iP is : ({iP2})
+Your Day is : ({"%s/%s/%s" % (e.day, e.month, e.year)})
+Your Time is : ({"%s:%s:%s" % (e.hour, e.minute, e.second)})
+
+*******************************************
+**
+
+User is : ({usr1})
+iNFO User is : ({a})
+
+By  Daddy HxN My instagram :  ( https://instagram.com/hxn.vb/ )
+
+My TeleGram : ( https://t.me/HEXiN1K )
+```
+        """
+
+
+#ضع الويب هوك الخاص بك اسفل هذا النص
+#Enter Your Webhook Below this text
+webhook = 'Your Webhook is here'
+
+
+datajs = {
+'content': message
+}
+ 
+req = requests.post(webhook,  data=datajs)
+
+url = "https://instagram.com/hxn.vb/"
+webbrowser.open(url)
